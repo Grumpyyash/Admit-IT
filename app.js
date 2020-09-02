9325ca9cb9d46009a547001752180435e21d5295
Yash vardhan <yashvardhan513@gmail.com>
2020-09-02 20:36:53 +0530

    update links

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secret",
+    callbackURL: "https://shielded-caverns-83501.herokuapp.com/auth/google/secret",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
